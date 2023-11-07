2e6b9bf454a7134f52ea9bfa45a856e9f40f8ea8
ahmed atia <77337808+Ahmed1010-1999@users.noreply.github.com>
2023-11-07 11:12:32 +0200

    Update deque_hacker_rank_problem_advanced.cpp

--- stl/deque_hacker_rank_problem_advanced.cpp
+++ stl/deque_hacker_rank_problem_advanced.cpp
@@ -5,38 +5,32 @@
 #include <algorithm>
 #include <deque>
 using namespace std;
+void max_arr(deque<int>& deqma, int noe, int san);
 
-void max_of_sub_arrs(deque<int>& deqma, int noe, int san);
 int main() {
-    int m;
     int num_of_query;
-    cin>>num_of_query;
-    deque<int> dq;
-    for (int q = 0; q < num_of_query; q++){
-        int num_of_element,sub_arr_n;
-        cin>>num_of_element>>sub_arr_n;
-        int s=num_of_element;
-        
-        for (;num_of_element>0;num_of_element-- ){
-            cin>>m;
+    cin >> num_of_query;
+
+    for (int q = 0; q < num_of_query; q++) {
+        int num_of_element, sub_arr_n;
+        cin >> num_of_element >> sub_arr_n;
+        deque<int> dq;
+
+        for (int i = 0; i < num_of_element; i++) {
+            int m;
+            cin >> m;
             dq.push_back(m);
         }
-        
-        max_of_sub_arrs(dq,s,sub_arr_n);
-        for (;s>0;s--){
-            cin>>m;
-            dq.pop_back();
-        }
-        num_of_query=num_of_query-1;
 
+        max_arr(dq, num_of_element, sub_arr_n);
     }
 }
-void max_of_sub_arrs(deque<int>&deqma,int noe,int san){
-    int m=noe-san+1;
-    for (int i=0;i<m;i++){
-        auto it=deqma.begin();
-        cout<<*max_element(it+i,it+i+san)<<' ';
-    }
-    cout << endl;  // Print a new line after each query's output
 
+void max_arr(deque<int>& deqma, int noe, int san) {
+    int m = noe - san + 1;
+    for (int i = 0; i < m; i++) {
+        auto it = deqma.begin();
+        cout << *max_element(it + i, it + i + san) << ' ';
+    }
+    cout << endl;
 }
